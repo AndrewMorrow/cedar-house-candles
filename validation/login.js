09d2b332385f53bc885aeda93aa9fb8e846d8ded
AndrewMorrow <beardedmongo@gmail.com>
2021-02-20 11:45:45 -0600

    Runs eslint to be rule compliant

--- validation/login.js
+++ validation/login.js
@@ -1,23 +1,25 @@
-const Validator = require('validator');
-const isEmpty = require('is-empty');
+const Validator = require("validator");
+const isEmpty = require("is-empty");
 
 module.exports = function validateLoginInput(data) {
-  let errors = {};
+    let errors = {};
 
-  Object.keys(data).forEach(key => {
-    data[key] = !isEmpty(data[key]) ? data[key] : '';
-  });
+    Object.keys(data).forEach(key => {
+        data[key] = !isEmpty(data[key]) ? data[key] : "";
+    });
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
-  }
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
 
-  if (Validator.isEmpty(data.password)) errors.password = 'Password field is required';
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }
 
-  return {
-    errors,
-    isValid: isEmpty(errors),
-  };
+    return {
+        errors,
+        isValid: isEmpty(errors),
+    };
 };
