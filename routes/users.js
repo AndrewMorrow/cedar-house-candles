0840d10f5d3bbbc27359911eb2f2671bd6d0fcc0
AndrewMorrow <beardedmongo@gmail.com>
2021-02-20 11:45:45 -0600

    Runs eslint to be rule compliant

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/me', (req, res) => {
-  const { _id, name, email, date } = req.user;
+router.get("/me", (req, res) => {
+    const { _id, name, email, date } = req.user;
 
-  return res.json({ _id, name, email, date });
+    return res.json({ _id, name, email, date });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 // Middleware packages
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // Routes
-const authRoutes = require('./routes/auth');
-const usersRoutes = require('./routes/users');
+const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users");
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,37 +20,37 @@ app.use(bodyParser.json());
 
 // Mongoose connection to MongoDB. (https://mongoosejs.com/docs/guide.html)
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.MONGODB_DATABASE}`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
+    process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.MONGODB_DATABASE}`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
 )
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
 // Passport JWT setup.
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Middleware to use when routes require authenticated user.
-const requiresAuth = passport.authenticate('jwt', { session: false });
+const requiresAuth = passport.authenticate("jwt", { session: false });
 
 // Login and register routes here don't require authenticated user.
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // For all authenticated routes, make sure to use this
-app.use('/api/users', requiresAuth, usersRoutes);
+app.use("/api/users", requiresAuth, usersRoutes);
 
 // For production, serve compiled React app in client build directory.
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
