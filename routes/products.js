2dddb30784d084801dd45c39c96709338e428587
AndrewMorrow <beardedmongo@gmail.com>
2021-02-24 12:55:16 -0600

    adds custom error handler and GET products route

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,19 @@
 import express from "express";
 const router = express.Router();
+import Product from "../models/ProductModel.js";
 
 // @desc        fetch all products
 // @route       GET /api/products
 // @access      Public
-router.get("/", (req, res) => {
-    console.log("All Products");
+router.get("/", async (req, res) => {
+    try {
+        console.log("All Products");
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(404);
+        throw new Error("Product Not Found");
+    }
 });
 
 export default router;
--- server.js
+++ server.js
@@ -10,14 +10,17 @@ import authRoutes from "./routes/auth.js";
 import usersRoutes from "./routes/users.js";
 import productRoutes from "./routes/products.js";
 import passConfig from "./config/passport.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // connect to mongo
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 // Middleware packages
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,6 +42,10 @@ app.use("/api/users", requiresAuth, usersRoutes);
 
 // Product data routes
 app.use("/api/products", productRoutes);
+
+// Custom Error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 // For production, serve compiled React app in client build directory.
 if (process.env.NODE_ENV === "production") {
