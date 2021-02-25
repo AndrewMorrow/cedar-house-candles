160660e8dba07a2d0cac684dbf3582446ebb2c5d
Santiago Hincapie <santiagohr81@hotmail.com>
2021-02-24 19:10:44 -0500

    added shoping page

---
+++ client/src/components/pages/Shop.js
@@ -0,0 +1,115 @@
+import React, {useState} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import Products from '../../../products.json';
+
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  cardAction: {
+    justifyContent:"center"
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  }
+}));
+
+
+
+export default function Album() {
+  const classes = useStyles();
+  
+  const [products, setproducts ] = useState(products);
+console.log(products)
+
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+ 
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Portfolio
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Below is a small sample of my work. See my GitHub for a more complete list of project and details.
+            </Typography>
+
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {products.map((card) => (
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={card.image}
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.name}
+                    </Typography>
+                    <Typography align="left">
+                    {card.description}
+                    <br/>
+                    <br/>
+                    {card.catrgory}
+                    </Typography>
+                  </CardContent>
+                  <CardActions className={classes.cardAction} >
+                  
+                   
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+  
+    </React.Fragment>
+  );
+}
