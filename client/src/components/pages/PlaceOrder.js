afee7284493f7a756faf6d291f3e208152829cbc
AndrewMorrow <beardedmongo@gmail.com>
2021-02-28 18:05:43 -0600

    Updates Place Order Screen

--- client/src/components/pages/PlaceOrder.js
+++ client/src/components/pages/PlaceOrder.js
@@ -12,6 +12,16 @@ import {
     ListItemText,
     ListItemIcon,
     Divider,
+    Paper,
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    IconButton,
+    Button,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
@@ -27,9 +37,24 @@ const useStyles = makeStyles((theme) => ({
     title: {
         // margin: theme.spacing(4, 0, 2),
     },
+    itemImage: {
+        height: "100px",
+    },
+    paperCon: {
+        padding: "20px",
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    quantNum: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    table: {
+        minWidth: 500,
+    },
 }));
 
-const PlaceOrder = () => {
+const PlaceOrder = ({ history }) => {
     const classes = useStyles();
     const { state, dispatch } = useContext(Store);
     const { cart } = state;
@@ -38,61 +63,151 @@ const PlaceOrder = () => {
         console.log(state.cart.paymentMethod);
     }, []);
 
+    function ccyFormat(num) {
+        return `${num.toFixed(2)}`;
+    }
+
+    function subtotal(items) {
+        return items
+            .map((item) => item.price * item.cartQty)
+            .reduce((sum, i) => sum + i, 8);
+    }
+
+    const invoiceSubtotal = subtotal(cart.cartItems);
+
+    const handleButtonClick = (newRoute) => {
+        history.push(newRoute);
+    };
+
     return (
         <main>
-            <Grid container justify="center">
-                <Grid item xs={12} md={6}>
-                    <Typography
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            marginBottom: "1rem",
-                        }}
-                        variant="h4"
-                        className={classes.title}
-                    >
-                        Order Items
-                    </Typography>
-
-                    <div className={classes.demo}>
-                        <List style={{ padding: "0" }}>
-                            {cart.cartItems.map((item) => (
-                                <ListItem style={{ borderBottom: "1px solid" }}>
-                                    <Link to={`product/${item._id}`}>
-                                        <img
-                                            src={item.image}
-                                            alt={item.description}
-                                            style={{
-                                                height: "40px",
-                                            }}
-                                        />
-                                    </Link>
-                                    <Link
-                                        style={{
-                                            color: "black",
-                                            fontweight: "bold",
-                                        }}
-                                        to={`product/${item._id}`}
-                                    >
-                                        <ListItemText
-                                            inset
-                                            primary={item.name}
-                                        />
-                                    </Link>
-                                    <ListItemText
-                                        inset
-                                        primary={item.itemProductType}
-                                    />
-                                    <ListItemText
-                                        inset
-                                        primary={`${item.cartQty} x $${
-                                            item.price
-                                        } = $${item.cartQty * item.price}  `}
-                                    />
-                                </ListItem>
-                            ))}
-                        </List>
-                    </div>
+            <Grid container>
+                <Grid container item sm={12}>
+                    <Container maxWidth="md">
+                        <TableContainer component={Paper}>
+                            <Table
+                                className={classes.table}
+                                aria-label="spanning table"
+                            >
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="center" colSpan={4}>
+                                            <Typography
+                                                variant="h4"
+                                                style={{
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    justifyContent: "center",
+                                                }}
+                                            >
+                                                Order Details
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell align="center">
+                                            <Typography variant="h6">
+                                                Image
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Typography variant="h6">
+                                                Name
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Typography variant="h6">
+                                                Qty.
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Typography variant="h6">
+                                                Price
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {cart.cartItems &&
+                                        cart.cartItems.length > 0 &&
+                                        cart.cartItems.map((item) => (
+                                            <>
+                                                <TableRow key={item._id}>
+                                                    <TableCell align="center">
+                                                        <Link
+                                                            to={`/product/${item._id}`}
+                                                        >
+                                                            <img
+                                                                className={
+                                                                    classes.itemImage
+                                                                }
+                                                                src={item.image}
+                                                                alt={
+                                                                    item.description
+                                                                }
+                                                            />
+                                                        </Link>
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        <div>
+                                                            <Typography variant="h6">
+                                                                {item.name}
+                                                            </Typography>
+                                                            <Typography variant="body1">
+                                                                {
+                                                                    item.itemProductType
+                                                                }
+                                                            </Typography>
+                                                        </div>
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        {item.cartQty}
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        ${ccyFormat(item.price)}
+                                                    </TableCell>
+                                                </TableRow>
+                                            </>
+                                        ))}
+                                    <TableRow>
+                                        <TableCell
+                                            rowSpan={1}
+                                            colSpan={2}
+                                            align="right"
+                                        >
+                                            Shipping
+                                        </TableCell>
+                                        <TableCell align="center">$8</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell rowSpan={3} />
+                                        <TableCell align="right" colSpan={1}>
+                                            Subtotal
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            ${ccyFormat(invoiceSubtotal)}
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Button
+                                                onClick={() =>
+                                                    handleButtonClick(
+                                                        `/ordersummary`
+                                                    )
+                                                }
+                                                disabled={
+                                                    cart.cartItems.length ===
+                                                        0 && true
+                                                }
+                                                variant="contained"
+                                            >
+                                                Place Order
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Container>
                 </Grid>
             </Grid>
         </main>
