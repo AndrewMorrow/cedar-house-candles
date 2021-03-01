8986beafbc0761aae5cc7f85222fe6e302f08442
AndrewMorrow <beardedmongo@gmail.com>
2021-02-28 23:48:38 -0600

    Adds order routes

--- routes/Order.js
+++ routes/Order.js
@@ -5,11 +5,18 @@ import { catchError } from "../middleware/errorMiddleware.js";
 
 // @desc        Create new Order
 // @route       POST /api/orders
-// @access      Private
+// @access      Public
 router.get(
     "/",
     catchError(async (req, res) => {
-        const { orderItems, paymentMethod } = req.body;
+        const {
+            orderItems,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            shippingPrice,
+            totalPrice,
+        } = req.body;
         if (orderItems && orderItems.length === 0) {
             res.status(400);
             throw new Error("no order items");
@@ -17,7 +24,11 @@ router.get(
             const order = new Order({
                 user: req.user._id,
                 orderItems,
+                shippingAddress,
                 paymentMethod,
+                itemsPrice,
+                shippingPrice,
+                totalPrice,
             });
 
             const createdOrder = await order.save();
