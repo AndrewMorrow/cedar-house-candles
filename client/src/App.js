f9f517c5e1eb0c3dbe7cf8ed9974360078b79069
Santiago Hincapie <santiagohr81@hotmail.com>
2021-03-04 19:08:04 -0500

    change the primary and secondary colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
-import React, { useContext, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./store/actions/authActions";
-import { Store } from "./store";
-import { CssBaseline } from "@material-ui/core";
-import "./App.css";
-import Footer from "./components/partials/Footer";
-import Navbar from "./components/partials/Navbar";
-import Landing from "./components/pages/Landing";
-import Register from "./components/pages/Register";
-import Login from "./components/pages/Login";
-import PrivateRoute from "./components/auth/PrivateRoute";
-import Dashboard from "./components/pages/Dashboard";
-import About from "./components/pages/About";
-import Shop from "./components/pages/Shop";
-import Cart from "./components/pages/Cart";
-import ProductDetail from "./components/pages/ProductDetail";
-import OrderDetail from "./components/pages/OrderDetail";
-import PlaceOrder from "./components/pages/PlaceOrder";
-import ShippingPage from "./components/pages/ShippingPage";
-import OrderThanks from "./components/pages/OrderThanks";
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './store/actions/authActions';
+import { Store } from './store';
+import { CssBaseline } from '@material-ui/core';
+import './App.css';
+import Footer from './components/partials/Footer';
+import Navbar from './components/partials/Navbar';
+import Landing from './components/pages/Landing';
+import Register from './components/pages/Register';
+import Login from './components/pages/Login';
+import PrivateRoute from './components/auth/PrivateRoute';
+import Dashboard from './components/pages/Dashboard';
+import About from './components/pages/About';
+import Shop from './components/pages/Shop';
+import Cart from './components/pages/Cart';
+import ProductDetail from './components/pages/ProductDetail';
+import OrderDetail from './components/pages/OrderDetail';
+import PlaceOrder from './components/pages/PlaceOrder';
+import ShippingPage from './components/pages/ShippingPage';
+import OrderThanks from './components/pages/OrderThanks';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#d8ac9c',
+        },
+        secondary: {
+            main: '#999b84',
+        },
+    },
+});
 
 const App = () => {
     const { dispatch } = useContext(Store);
@@ -38,37 +52,42 @@ const App = () => {
 
             if (decoded.exp < currentTime) {
                 dispatch(logoutUser());
-                window.location.href = "./login";
+                window.location.href = './login';
             }
         }
     }, [dispatch]);
 
     return (
         <Router>
-            <CssBaseline>
-                <div className="App">
-                    <Navbar />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/shop" component={Shop} />
-                    <Route path="/product/:id" component={ProductDetail} />
-                    <Route path="/order/:id" component={OrderDetail} />
-                    <Route path="/checkout" component={ShippingPage} />
-                    <Route path="/cart" component={Cart} />
-                    <Route path="/orderthanks/:id" component={OrderThanks} />
-                    <Route path="/placeorder" component={PlaceOrder} />
-                    <Switch>
-                        <PrivateRoute
-                            exact
-                            path="/dashboard"
-                            component={Dashboard}
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                    <div className="App">
+                        <Navbar />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/shop" component={Shop} />
+                        <Route path="/product/:id" component={ProductDetail} />
+                        <Route path="/order/:id" component={OrderDetail} />
+                        <Route path="/checkout" component={ShippingPage} />
+                        <Route path="/cart" component={Cart} />
+                        <Route
+                            path="/orderthanks/:id"
+                            component={OrderThanks}
                         />
-                    </Switch>
-                    <Footer />
-                </div>
-            </CssBaseline>
+                        <Route path="/placeorder" component={PlaceOrder} />
+                        <Switch>
+                            <PrivateRoute
+                                exact
+                                path="/dashboard"
+                                component={Dashboard}
+                            />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </CssBaseline>
+            </ThemeProvider>
         </Router>
     );
 };
