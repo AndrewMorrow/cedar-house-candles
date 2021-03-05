55d980d78d4ce3b333c857b803ee9ebcdc288f4c
Santiago Hincapie <santiagohr81@hotmail.com>
2021-03-05 18:23:51 -0500

    landing pag style updates

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,54 +1,50 @@
-import React, { useContext, useEffect } from "react";
-import { Store } from "../../store";
+import React, { useContext, useEffect } from 'react';
+import { Store } from '../../store';
 // import { Link } from 'react-router-dom';
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import { getProducts } from "../../store/actions/productActions";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import { Container, Paper, Tooltip } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { getProducts } from '../../store/actions/productActions';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { Container, Paper, Tooltip } from '@material-ui/core';
 
-import ListSubheader from "@material-ui/core/ListSubheader";
-import IconButton from "@material-ui/core/IconButton";
-import InfoIcon from "@material-ui/icons/Info";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 const useStyles = makeStyles((theme) => ({
     main: {
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        overflow: "hidden",
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
 
-        alignItems: "center",
+        alignItems: 'center',
     },
     gridList: {
         width: 600,
     },
     container: {
-        display: "flex",
-        alignItems: "center",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        overflow: "hidden",
-        paddingLeft: "0",
-        paddingRight: "0",
+        display: 'flex',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        padding: '1rem',
     },
     icon: {
-        color: "rgba(255, 255, 255, 0.54)",
+        color: 'rgba(255, 255, 255, 0.54)',
     },
     title: {
         margin: theme.spacing(4, 0, 2),
-        fontFamily: "Old Standard TT",
-        color: "#214c1f",
+        fontFamily: 'Old Standard TT',
+        color: '#214c1f',
     },
     paper: {
-        margin: "auto",
-        maxWidth: 950,
-        backgroundColor: "#efd9d1",
-        borderRadius: "1.25rem",
-
+        backgroundColor: '#efd9d1',
+        borderRadius: '1.25rem',
         marginTop: theme.spacing(2),
         marginBottom: theme.spacing(2),
         padding: theme.spacing(1),
@@ -57,6 +53,14 @@ const useStyles = makeStyles((theme) => ({
             marginBottom: theme.spacing(6),
             padding: theme.spacing(4),
         },
+    },
+    infoWrapper: {
+        border: '3px double',
+        borderRadius: '15px',
+        padding: '1rem',
+        marginBottom: '2rem',
+        backgroundColor: '#f4eeed',
+        maxWidth: '800px',
     },
 }));
 
@@ -83,48 +87,102 @@ const Landing = (props) => {
         <main className={classes.main}>
             <Container>
                 <Paper className={classes.paper} elevation10>
-                    <Container maxWidth="xs">
-                        <Typography
-                            variant="h2"
-                            component="h2"
-                            align="center"
-                            className={classes.title}
-                        >
-                            <b>Autumn Collection</b>
-                        </Typography>
-                    </Container>
-                    <Container className={classes.container}>
-                        <GridList
-                            cellHeight={160}
-                            className={classes.gridList}
-                            cols={3}
-                        >
-                            {products &&
-                                products.length > 0 &&
-                                products
-                                    .filter(
-                                        (item) =>
-                                            item.productCollection === "Autumn"
-                                    )
-                                    .map((product) => (
-                                        <GridListTile
-                                            key={product.image}
-                                            cols={product.cols || 1}
-                                        >
+                    <div className={classes.infoWrapper}>
+                        <Container>
+                            <Typography
+                                variant="h2"
+                                component="h2"
+                                align="center"
+                                className={classes.title}
+                            >
+                                <b>Autumn Collection</b>
+                            </Typography>
+                        </Container>
+                        <Container className={classes.container}>
+                            <GridList
+                                cellHeight={160}
+                                className={classes.gridList}
+                                cols={3}
+                            >
+                                {products &&
+                                    products.length > 0 &&
+                                    products
+                                        .filter(
+                                            (item) =>
+                                                item.productCollection ===
+                                                'Autumn'
+                                        )
+                                        .map((product) => (
+                                            <GridListTile
+                                                key={product.image}
+                                                cols={product.cols || 1}
+                                            >
+                                                <img
+                                                    src={product.image}
+                                                    alt={product.name}
+                                                />
+                                                <GridListTileBar
+                                                    title={product.name}
+                                                    actionIcon={
+                                                        <IconButton
+                                                            onClick={() =>
+                                                                handleButtonClick(
+                                                                    `/product/${product._id}`
+                                                                )
+                                                            }
+                                                            aria-label={`info about ${product.name}`}
+                                                            className={
+                                                                classes.icon
+                                                            }
+                                                        >
+                                                            <InfoIcon />
+                                                        </IconButton>
+                                                    }
+                                                />
+                                            </GridListTile>
+                                        ))}
+                            </GridList>
+                        </Container>
+                    </div>
+                    <div className={classes.infoWrapper}>
+                        <Container>
+                            <Typography
+                                variant="h2"
+                                component="h2"
+                                align="center"
+                                className={classes.title}
+                            >
+                                <b>Best Sellers</b>
+                            </Typography>
+                        </Container>
+                        <Container className={classes.container}>
+                            <GridList
+                                cellHeight={180}
+                                className={classes.gridList}
+                            >
+                                <GridListTile
+                                    key="Subheader"
+                                    cols={2}
+                                    style={{ height: 'auto' }}
+                                ></GridListTile>
+                                {products
+                                    .filter((item) => item.isBestSeller)
+                                    .map((tile) => (
+                                        <GridListTile key={tile.image}>
                                             <img
-                                                src={product.image}
-                                                alt={product.name}
+                                                src={tile.image}
+                                                alt={tile.name}
                                             />
                                             <GridListTileBar
-                                                title={product.name}
+                                                title={tile.name}
                                                 actionIcon={
                                                     <IconButton
                                                         onClick={() =>
                                                             handleButtonClick(
-                                                                `/product/${product._id}`
+                                                                `/product/${tile._id}`
                                                             )
                                                         }
-                                                        aria-label={`info about ${product.name}`}
+                                                        aria-label={`info about ${tile.name}`}
                                                         className={classes.icon}
                                                     >
                                                         <InfoIcon />
@@ -133,50 +191,9 @@ const Landing = (props) => {
                                             />
                                         </GridListTile>
                                     ))}
-                        </GridList>
-                    </Container>
-                    <Container>
-                        <Typography
-                            variant="h2"
-                            component="h2"
-                            align="center"
-                            className={classes.title}
-                        >
-                            <b>Best Sellers</b>
-                        </Typography>
-                    </Container>
-                    <Container className={classes.container}>
-                        <GridList cellHeight={180} className={classes.gridList}>
-                            <GridListTile
-                                key="Subheader"
-                                cols={2}
-                                style={{ height: "auto" }}
-                            ></GridListTile>
-                            {products
-                                .filter((item) => item.isBestSeller)
-                                .map((tile) => (
-                                    <GridListTile key={tile.image}>
-                                        <img src={tile.image} alt={tile.name} />
-                                        <GridListTileBar
-                                            title={tile.name}
-                                            actionIcon={
-                                                <IconButton
-                                                    onClick={() =>
-                                                        handleButtonClick(
-                                                            `/product/${tile._id}`
-                                                        )
-                                                    }
-                                                    aria-label={`info about ${tile.name}`}
-                                                    className={classes.icon}
-                                                >
-                                                    <InfoIcon />
-                                                </IconButton>
-                                            }
-                                        />
-                                    </GridListTile>
-                                ))}
-                        </GridList>
-                    </Container>
+                            </GridList>
+                        </Container>
+                    </div>
                 </Paper>
             </Container>
         </main>
